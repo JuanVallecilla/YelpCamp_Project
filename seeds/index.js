6ea611a8367ea643124f34b024918b4dd0ca4597
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-04-13 20:23:08 -0400

    Finish basic styles and layouts bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,14 @@ const seedDB = async () => {
   for (let i = 0; i < 50; i++) {
     //Doing 1000 because we have 1000 cities in the cities.js array
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city} , ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta lacus libero, sagittis volutpat enim porta sed. Mauris libero nibh, posuere at porttitor eget, commodo sit amet risus. Integer auctor, sem auctor auctor porta, leo massa volutpat leo, non lobortis urna erat sed dolor. Donec sodales mollis leo non vulputate. Pellentesque non mi faucibus, tincidunt lorem vestibulum, luctus neque. Aenean tincidunt, justo non varius sagittis, purus quam posuere elit, in fermentum lacus lorem eget velit. Cras quis est nec odio dapibus tempor. Duis ultrices porta tempus.",
+      price,
     });
     await camp.save();
   }
