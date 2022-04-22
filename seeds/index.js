e51a037fb5348381f88e45df3f5f1a3013e9c043
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-04-22 17:14:37 -0400

    Showing and Hiding Edit/Delete

--- seeds/index.js
+++ seeds/index.js
@@ -24,7 +24,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-      author: "626105503264fce0914189b9",
+      author: "626318eabdaaff83c60ef1cc",
       location: `${cities[random1000].city} , ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/collection/483251",
