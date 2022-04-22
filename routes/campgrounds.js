73553a10adc24ca73e30f251cbad30638abb5341
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-04-22 17:39:14 -0400

    Inital logic for Permissions

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -68,6 +68,10 @@ router.get(
       req.flash("error", "Cannot find campground");
       return res.redirect("/campgrounds");
     }
+    if (!campground.author.equals(req.user._id)) {
+      req.flash("error", "You do not have permission to do that!");
+      return res.redirect(`/campgrounds/${id}`);
+    }
     res.render("campgrounds/edit", { campground });
   })
 );
@@ -78,7 +82,12 @@ router.put(
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+      req.flash("error", "You do not have permission to do that!");
+      return res.redirect(`/campgrounds/${id}`);
+    }
+    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash("success", "Successfully Updated Campground!");
     res.redirect(`/campgrounds/${campground._id}`);
   })
