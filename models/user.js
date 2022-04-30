f3884da81a5e54d9dc146bf0b5c2928f1c4044e0
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-04-30 15:32:47 -0400

    refactor user  model to accept more fields

--- models/user.js
+++ models/user.js
@@ -1,13 +1,38 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
 const passportLocalMongoose = require("passport-local-mongoose");
 
+const ImageProfileSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageProfileSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/ar_4:3,c_crop");
+});
+
 const UserSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  avatar: [ImageProfileSchema],
+  phone: String,
+
   email: {
     type: String,
     require: true,
     unique: true,
   },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
 UserSchema.plugin(passportLocalMongoose);
