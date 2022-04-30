5cf329c3e0364b0d512c3eb2ecb671625cae8c3f
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-04-30 15:32:07 -0400

    Updating  user profile logic and sign up logic

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,7 @@
 const User = require("../models/user");
+const { cloudinary } = require("../cloudinary");
+const Campground = require("../models/campground");
+const Review = require("../models/review");
 
 module.exports.renderRegister = (req, res) => {
   res.render("users/register");
@@ -6,8 +9,17 @@ module.exports.renderRegister = (req, res) => {
 
 module.exports.register = async (req, res, next) => {
   try {
-    const { email, username, password } = req.body;
-    const user = new User({ email, username });
+    const { email, username, password, firstName, lastName, phone, avatar } = req.body;
+    const user = new User({ email, username, firstName, lastName, phone, avatar });
+
+    // if user does not  submit an avatar image we give him a default avatar
+    if (!req.file) {
+    } else {
+      user.avatar = {
+        url: req.file.path,
+        filename: req.file.filename,
+      };
+    }
     const registerUser = await User.register(user, password);
     // Allows the user to automatically ne log-in after registering
     req.login(registerUser, (err) => {
@@ -36,4 +48,24 @@ module.exports.logout = (req, res) => {
   req.logout();
   req.flash("success", "Goodbye");
   res.redirect("/campgrounds");
+};
+
+module.exports.userProfile = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+
+  if (!user) {
+    req.flash("error", "Cannot find User!");
+    return res.redirect("/campgrounds");
+  }
+
+  const campground = await Campground.find().where("author").equals(user._id);
+  const reviews = await Review.find().where("user").equals(user._id);
+  // const campground = await Campground.find().where("author").equals(req.user._id);
+  // const reviews = await Review.find().where("user").equals(req.user._id);
+  if (!campground) {
+    req.flash("error", "Cannot find Campgrounds!");
+    return res.redirect("/campgrounds");
+  }
+  res.render("users/profile", { user, campgrounds: campground, reviews: reviews });
 };
