cedeb6328ba7cc93298a20d4288b41efb84bdc85
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-05-15 06:08:25 -0400

    remove duplicate  route

--- routes/admin.js
+++ routes/admin.js
@@ -7,8 +7,8 @@ const users = require("../controllers/admin");
 const admin = require("../controllers/admin");
 const { isLoggedIn, checkProfileOwner, isAdmin, validateEditUser, validateEditAdmin } = require("../middleware");
 
-router.route("/admin").get(isAdmin, catchAsync(admin.userAdmin));
+router.route("/admin").get(isLoggedIn, isAdmin, catchAsync(admin.userAdmin));
 
-router.route("/users/:id").put(isAdmin, validateEditAdmin, catchAsync(admin.updateAdmin));
+// router.route("/users/:id").put(isAdmin, validateEditAdmin, catchAsync(admin.updateAdmin));
 
 module.exports = router;
