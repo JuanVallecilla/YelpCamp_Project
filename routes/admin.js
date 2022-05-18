9d275d5af5f6fbcda224844f56de4cb61c942a5a
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-05-18 10:48:37 -0400

    fixed admin route

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("passport");
 const catchAsync = require("../utils/catchAsync");
-const User = require("../models/user");
-const users = require("../controllers/admin");
 const admin = require("../controllers/admin");
-const { isLoggedIn, checkProfileOwner, isAdmin, validateEditUser, validateEditAdmin } = require("../middleware");
+const { isLoggedIn, checkProfileOwner, isAdmin } = require("../middleware");
 
 router.route("/admin").get(isLoggedIn, isAdmin, catchAsync(admin.userAdmin));
 
-// router.route("/users/:id").put(isAdmin, validateEditAdmin, catchAsync(admin.updateAdmin));
+router.route("/admin/:id").put(isAdmin, checkProfileOwner, catchAsync(admin.updateAdmin));
 
 module.exports = router;
